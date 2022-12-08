13fd84f7d5600e32bfa57b7c58c220d6f6b658bd
Mario Mata <mmatas1300@gmail.com>
2022-12-08 17:49:15 -0600

    Add fonts-background

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,20 @@
 //Componente de inicio
 export function App() {
   return(
+  <div> 
     <header>
       <h1>die Wortschatzkartei</h1>
     </header>
+    <main>
+      <ul className="mainMenu">
+        <li>A1</li>
+        <li>A2</li>
+        <li>B1</li>
+      </ul>
+    </main>
+    <footer>
+      2023
+    </footer>
+  </div> 
   );
 }
