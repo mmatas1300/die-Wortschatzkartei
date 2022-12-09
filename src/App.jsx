92b674cef5015ad27c05cb0067d8d546e9d3a737
Mario Mata <mmatas1300@gmail.com>
2022-12-08 23:16:08 -0600

    Add main grid

--- src/App.jsx
+++ src/App.jsx
@@ -6,15 +6,38 @@ export function App() {
       <h1>die Wortschatzkartei</h1>
     </header>
     <main>
+
       <div className="mainMenu">
-        <ul className="levelCard lcA1">
-          <li>A1</li>
-          <li className="levelTopics">Lebensmittel</li>
-          <li className="levelTopics">Berufe</li>
-        </ul>
-        <ul className="levelCard lcA2">A2</ul>
-        <ul className="levelCard lcB1">B1</ul>
+
+        <div className="levelCard lcA1">
+          <p>A1</p>
+          <ul className="levelTopics">
+            <li>Lebensmittel</li>
+            <li>Berufe</li>
+          </ul>
+        </div>
+
+
+
+        <div className="levelCard lcA2">
+          <p>A2</p>
+          <ul className="levelTopics">
+            <li>Lebensmittel</li>
+            <li>Berufe</li>
+          </ul>
+        </div>
+
+
+
+        <div className="levelCard lcB1">
+          <p>B1</p>
+          <ul className="levelTopics">
+
+          </ul>
+        </div>
       </div>
+
+
     </main>
     <footer>
       2023
