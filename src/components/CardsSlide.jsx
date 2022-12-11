4c927ecbc1a9cf5c09521295990cd02191e90628
Mario Mata <mmatas1300@gmail.com>
2022-12-10 19:02:20 -0600

    Add cards style

--- src/components/CardsSlide.jsx
+++ src/components/CardsSlide.jsx
@@ -1,29 +1,42 @@
-
+import styles from "./CardsSlide.module.css";
 
 export function CardsSlide({card}){
-    return <div>
+    
+    if(card.type=="Nomen-das"){
+        var cardStyleColor = styles.karteNomenDas;
+    }else if(card.type=="Nomen-der"){
+        cardStyleColor = styles.karteNomenDer;
+    }else if(card.type=="Nomen-die"){
+        cardStyleColor = styles.karteNomenDie;
+    }else if(card.type=="Nomen-2"){
+        cardStyleColor = styles.karteNomen2;
+    }else if(card.type=="Verbe"){
+        cardStyleColor = styles.karteVerbe;
+    }
+
+    return <div className={`${cardStyleColor} ${styles.karte}`}>
         
-        <li>{card.typ}</li>
+        <li className={styles.typ}>{card.typ}</li>
 
-        <li>{card.sg}</li>
-        <li>{card.pl}</li>
+        <li className={styles.sg}>{card.sg}</li>
+        <li className={styles.pl}>{card.pl}</li>
 
 
-        <li>{card.infinitiv}</li>
-        <li>{card.präsens1}</li>
-        <li>{card.präsens2}</li>
-        <li>{card.präsens3}</li>
-        <li>{card.präsens4}</li>
-        <li>{card.präsens5}</li>
-        <li>{card.präsens5}</li>
+        <li className={styles.infinitiv}>{card.infinitiv}</li>
+        <li className={styles.präsens1}>{card.präsens1}</li>
+        <li className={styles.präsens2}>{card.präsens2}</li>
+        <li className={styles.präsens3}>{card.präsens3}</li>
+        <li className={styles.präsens4}>{card.präsens4}</li>
+        <li className={styles.präsens5}>{card.präsens5}</li>
+        <li className={styles.präsens6}>{card.präsens6}</li>
 
-        <li>{card.mann}</li>
-        <li>{card.manner}</li>
-        <li>{card.frau}</li>
-        <li>{card.frauen}</li>
+        <li className={styles.mann}>{card.mann}</li>
+        <li className={styles.manner}>{card.manner}</li>
+        <li className={styles.frau}>{card.frau}</li>
+        <li className={styles.frauen}>{card.frauen}</li>
 
-        <li>{card.verwandte}</li>
-        <li>{card.beispiel}</li>
+        <li className={styles.verwandte}>{card.verwandte}</li>
+        <li className={styles.beispiel}>{card.beispiel}</li>
 
     </div>;
 }
--- src/components/CardsSlideGrid.jsx
+++ src/components/CardsSlideGrid.jsx
@@ -1,12 +1,13 @@
 import { useParams } from "react-router-dom";
 import { CardsSlide } from "./CardsSlide.jsx";
+import styles from "./CardsSlideGrid.module.css";
 
 export function CardsSlideGrid(){
 
     const {letterId} =useParams();
     const wortschatzkartei = require("./Wortschatzkartei.json");
     const filterWortschatzkartei =wortschatzkartei.filter(e => e.anfangsbuchstabe == letterId);  
-    return (<ul>
+    return (<ul className={styles.cardsGrid}>
         {filterWortschatzkartei.map((card) => (<CardsSlide card={card}/>))}
     </ul>
     );
