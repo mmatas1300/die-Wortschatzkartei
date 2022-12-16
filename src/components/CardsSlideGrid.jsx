ec489020ce136c8843483aed5e6a526c8dcd1ece
Mario Mata <mmatas1300@gmail.com>
2022-12-16 16:32:24 -0600

    Change time reveal

--- src/components/CardsSlideGrid.jsx
+++ src/components/CardsSlideGrid.jsx
@@ -8,7 +8,7 @@ export function CardsSlideGrid(){
     const wortschatzkartei = require("./Wortschatzkartei.json");
     const filterWortschatzkartei =wortschatzkartei.filter(e => e.anfangsbuchstabe === letterId);
     filterWortschatzkartei.sort(( a, b ) => {return a.wort < b.wort ? -1 : a.wort > b.wort ? 1 : 0});
-    return (<Fade cascade damping={0.1} triggerOnce><ul className={styles.cardsGrid}>
+    return (<Fade cascade damping={0.01} triggerOnce><ul className={styles.cardsGrid}>
         {filterWortschatzkartei.map((card) => (<CardsSlide key={card.id} card={card}/>))}
     </ul></Fade>
     );
