0c7c93fa8065541000b1b09d8eec2abdcb2b6584
Mario Mata <mmatas1300@gmail.com>
2022-12-16 18:29:18 -0600

    Remove q,x,y

--- src/components/MenuGrid.jsx
+++ src/components/MenuGrid.jsx
@@ -5,7 +5,7 @@ import { Fade } from "react-awesome-reveal";
 
 export function MenuGrid(){
 
-    const alph =["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
+    const alph =["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","R","S","T","U","V","W","Z"];
 
     return (<Fade triggerOnce><ul className={styles.menuGrid}>
         {alph.map((letter) => (<CardMenuGrid key={letter} letter={letter}/>))}
