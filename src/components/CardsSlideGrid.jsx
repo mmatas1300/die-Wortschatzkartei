df393b8148eee19c06b63cd92aaba62ab25a89a7
Mario Mata <mmatas1300@gmail.com>
2022-12-24 09:28:26 -0600

    Add import-from

--- src/components/CardsSlideGrid.jsx
+++ src/components/CardsSlideGrid.jsx
@@ -2,11 +2,11 @@ import { useParams } from "react-router-dom";
 import { CardsSlide } from "./CardsSlide.jsx";
 import styles from "./CardsSlideGrid.module.css";
 import { Fade } from "react-awesome-reveal";
+import wortschatzkartei from "./Wortschatzkartei.json";
 
 
 export function CardsSlideGrid(){
     const {letterId} =useParams();
-    const wortschatzkartei = require("./Wortschatzkartei.json");
     const filterWortschatzkartei =wortschatzkartei.filter(e => e.anfangsbuchstabe === letterId);
     filterWortschatzkartei.sort(( a, b ) => {return a.wort < b.wort ? -1 : a.wort > b.wort ? 1 : 0});
     return (<Fade cascade damping={0.02} triggerOnce><ul className={styles.cardsGrid}>
