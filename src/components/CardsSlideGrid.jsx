751cd78b348f40af5c71929d064aaec9fddaf90e
Mario Mata <mmatas1300@gmail.com>
2022-12-24 11:08:35 -0600

    Add fetch - API

--- src/components/CardsSlideGrid.jsx
+++ src/components/CardsSlideGrid.jsx
@@ -2,15 +2,23 @@ import { useParams } from "react-router-dom";
 import { CardsSlide } from "./CardsSlide.jsx";
 import styles from "./CardsSlideGrid.module.css";
 import { Fade } from "react-awesome-reveal";
-import wortschatzkartei from "./Wortschatzkartei.json";
-
+import { useState, useEffect } from 'react';
 
 export function CardsSlideGrid(){
     const {letterId} =useParams();
-    const filterWortschatzkartei =wortschatzkartei.filter(e => e.anfangsbuchstabe === letterId);
-    filterWortschatzkartei.sort(( a, b ) => {return a.wort < b.wort ? -1 : a.wort > b.wort ? 1 : 0});
+    const [karten, setKarten] = useState([])  
+    useEffect(() => {
+    fetch("https://die-wortschatzkartei-default-rtdb.firebaseio.com/.json").then((response) => {
+        return response.json()
+      })
+      .then((karten) => {
+        setKarten(karten)
+      })
+  }, [])
+    const Wortschatzkartei =karten.filter(e => e.anfangsbuchstabe === letterId);
+    Wortschatzkartei.sort(( a, b ) => {return a.wort < b.wort ? -1 : a.wort > b.wort ? 1 : 0});
     return (<Fade cascade damping={0.02} triggerOnce><ul className={styles.cardsGrid}>
-        {filterWortschatzkartei.map((card) => (<CardsSlide key={card.id} card={card}/>))}
+        {Wortschatzkartei.map((card) => (<CardsSlide key={card.id} card={card}/>))}
     </ul></Fade>
     );
 }
