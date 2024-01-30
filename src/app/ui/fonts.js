2272349fad590aa317023ddd47b15ba7c151e824
Mario Mata <mmatas1300@gmail.com>
2024-01-30 08:32:51 -0600

    remove @next/font

--- src/app/ui/fonts.js
+++ src/app/ui/fonts.js
@@ -1,4 +1,4 @@
-import {Baloo_Bhaijaan_2} from '@next/font/google'
+import {Baloo_Bhaijaan_2} from 'next/font/google'
 
 export const baloo = Baloo_Bhaijaan_2({ weight: ['500'],
 subsets: ['latin'], });
