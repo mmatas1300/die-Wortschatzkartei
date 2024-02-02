e53e4eb74c01776c47441f2e8835f7bd3c44bb96
Mario Mata <mmatas1300@gmail.com>
2024-02-01 21:55:50 -0600

    add style in nomen karten

--- src/app/uben/page.jsx
+++ src/app/uben/page.jsx
@@ -1,10 +1,22 @@
 import Karte from "@/components/Karte";
 
+
 function UbenPage() {
+
+    const myKarte ={
+        type:"Nomen-die",
+        wort:"Ananas",
+        plural:"Ananase",
+        bild:"https://previews.123rf.com/images/aquir/aquir2306/aquir230600648/207729001-pi%C3%B1a-pi%C3%B1a-dibujado-a-mano-ilustraci%C3%B3n-vector-doodle-estilo-dibujos-animados-ilustraci%C3%B3n.jpg",
+        verwandte:"das Essen | das Obst",
+        beispiel:"Die Ananas wurde von den Ureinwohnerinnen angepflanzt.",
+        ubersetzung:"Pineapple | Piña"
+    }
+
     return(
         <div>
             <div className="flex flex-col justify-center items-center mt-12">
-                <Karte />
+                <Karte {...myKarte}/>
             </div>
         </div>
     );
--- src/components/Karte.jsx
+++ src/components/Karte.jsx
@@ -3,29 +3,86 @@ import ReactCardFlip from 'react-card-flip';
 import { useState } from 'react';
 import style from '@/components/Karte.module.css'
 
-function Karte() {
+
+function Karte(karte) {
 
     const [isFlipped, setIsFlipped] = useState(false);
 
+    const [showUbersetzung, setShowUbersetzung] = useState(false);
+
+    const toggleUbersetzung = () =>{
+        setShowUbersetzung(true);
+    };
+
+    const colorKarte = () => {
+        switch (karte.type) {
+            case "Nomen-das":
+                return style.karteDas;
+            case "Nomen-der":
+                return style.karteDer;
+            case "Nomen-die":
+                return style.karteDie;
+            case "Nomen-pl":
+                return style.kartePl;
+            case "Nomen-MUF":
+                return style.karteMUF;
+            case "Verb":
+                return style.karteVerb;
+            default:
+                return style.karteAndere;
+        }
+    };
+
+    const typeKarte = () => {
+        switch (karte.type) {
+            case "Nomen-das":
+            case "Nomen-der":
+            case "Nomen-die":
+            case "Nomen-pl":
+            case "Nomen-MUF":
+                return "Nomen";
+            case "Verb":
+                return "Verb";
+            default:
+                return karte.type;
+        }
+    };
+
+    const wortKarte = () => {
+        switch (karte.type) {
+            case "Nomen-das":
+                return "das " + karte.wort;
+            case "Nomen-der":
+                return "der " + karte.wort;
+            case "Nomen-die":
+                return "die " + karte.wort;
+            case "Nomen-pl":
+                return "die " + karte.wort;
+            case "Nomen-MUF":
+                return "der " + karte.wort;
+            default:
+                return karte.wort;
+        }
+    };
 
     return (
         <div className='flex flex-col justify-center items-center'>
             <ReactCardFlip isFlipped={isFlipped} flipDirection="horizontal">
-                <div className={`${style.karte} flex  flex-col justify-center items-center`}>
-                    das Kind
+                <div className={`${style.karte} flex  flex-col justify-center items-center text-xl ${colorKarte()}`}>
+                    {wortKarte()}
                 </div>
+                <div className={`${style.karte} flex  flex-col justify-center items-center ${colorKarte()}`}>
+                    <p className="self-end me-4 mt-4">{typeKarte()}</p>
+                    <p className='text-xl'>{wortKarte()}</p>
+                    <p>{karte.plural ? "die " + karte.plural : ""}</p>
 
-                <div className={`${style.karte} flex  flex-col justify-center items-center`}>
-                    <p>Nomen</p>
-                    <p>das Kind</p>
-                    <p>die Kinder</p>
-                    <p>bild</p>
-                    <p>das Mädchen</p>
-                    <p>Er ist ein Kind</p>
-                    <p>Kid | Niño</p>
+                    <img src={karte.bild} alt={wortKarte()} className='w-60 rounded-2xl my-4' />
+                    <p className='mb-4'>{karte.verwandte}</p>
+                    <p className='mx-4 mb-4'>{karte.beispiel}</p>
+                    <p onClick={toggleUbersetzung} className='mb-4 bg-gray-900 p-1 rounded-md cursor-pointer text-sm'>{showUbersetzung?karte.ubersetzung:"Übersetzung"}</p>
                 </div>
             </ReactCardFlip>
-            <button className={`${style.flipButton} ${isFlipped? "hidden" : ""}`} onClick={()=>{setIsFlipped(true)}}>Umdrehen</button>   
+            <button className={`${style.flipButton} ${isFlipped ? "hidden" : ""}`} onClick={() => { setIsFlipped(true) }}>Umdrehen</button>
 
         </div>
 
