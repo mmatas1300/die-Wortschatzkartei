85fed60c9017c43c9ced9e71bd03be30dbe0fd97
Mario Mata <mmatas1300@gmail.com>
2024-03-17 18:08:41 -0600

    style: add underline in cards

--- src/components/KarteWorterbuch.jsx
+++ src/components/KarteWorterbuch.jsx
@@ -66,7 +66,7 @@ function Karte(karte) {
     return (
         <div className='flex flex-col justify-center items-center transition duration-200 hover:scale-105'>
                 <div className={`w-80 min-h-80 rounded-3xl flex flex-col justify-center items-center text-xl ${colorKarte()}`}>
-                    <p className="self-end me-4 mt-4">{typeKarte()}</p>
+                    <p className="underline self-end me-4 mt-4">{typeKarte()}</p>
                     {karte.type === "Nomen-MUF"?(
                         <div className='grid grid-cols-2 justify-items-center'>
                             <p className='mx-2 text-base' >{wortKarte()}</p>
@@ -78,7 +78,7 @@ function Karte(karte) {
                     
                     <p>{karte.plural ? "die " + karte.plural : ""}</p>
                     {karte.type === "Verb" ?(<div className='flex  flex-col justify-center items-center'>
-                    <p className='mt-2'>Präsens</p>
+                    <p className='underline mt-2'>Präsens</p>
                     <div class="grid grid-cols-2 justify-items-center">
                         <p className='mx-1 text-sm'>{karte.prasens[0]}</p>
                         <p className='mx-1 text-sm'>{karte.prasens[3]}</p>
@@ -88,7 +88,7 @@ function Karte(karte) {
                         <p className='mx-1 text-sm'>{karte.prasens[5]}</p>
                     </div>
 
-                    <p className='mt-2'>{(karte.prateritum.length !== 0) ? "Präteritum" : ""}</p>
+                    <p className='underline mt-2'>{(karte.prateritum.length !== 0) ? "Präteritum" : ""}</p>
                     <div class="grid grid-cols-2 justify-items-center">
                         <p className='mx-1 text-sm'>{karte.prateritum[0]}</p>
                         <p className='mx-1 text-sm'>{karte.prateritum[3]}</p>
@@ -98,14 +98,14 @@ function Karte(karte) {
                         <p className='mx-1 text-sm'>{karte.prateritum[5]}</p>
                     </div>
 
-                    <p className='mt-2'>{karte.type === "Verb" ? "Partizip II" : ""}</p>
+                    <p className='underline mt-2'>{karte.type === "Verb" ? "Partizip II" : ""}</p>
                     <p className='text-sm'>{karte.partizip2}</p></div>) : ""}
                     
 
                     <img src={karte.bild} alt={wortKarte()} className='w-60 rounded-2xl my-4' />
                     <p className='mb-4'>{karte.verwandte}</p>
                     <p className='mx-4 mb-4'>{karte.beispiel}</p>
-                    <p onClick={toggleUbersetzung} className='mb-4 bg-gray-900 p-1 rounded-md cursor-pointer text-sm'>{showUbersetzung ? karte.ubersetzung : "Übersetzung"}</p>
+                    <p onClick={toggleUbersetzung} className='mb-4 bg-black-card p-1 rounded-md cursor-pointer text-sm'>{showUbersetzung ? karte.ubersetzung : "Übersetzung"}</p>
                 </div>
         </div>
 
