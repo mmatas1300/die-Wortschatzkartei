273432984c6c5939899aa2b4c6d2b665c4c1c745
Mario Mata <mmatas1300@gmail.com>
2024-04-01 18:00:23 -0600

    remove progress in user schema

--- src/models/user.js
+++ src/models/user.js
@@ -3,7 +3,6 @@ import { Schema, model, models } from "mongoose";
 const userSchema = new Schema({
     email: String,
     password:String,
-    progress: [{cardId: Schema.Types.ObjectId, phase: Number, date: Date}],
     myCards:[Schema.Types.Mixed],
     config:{nick:String, cardsSet:String},
 })
