a50fc2da112679b3fff3419f19fcaf5bb0f675f8
Mario Mata <mmatas1300@gmail.com>
2024-04-25 18:45:11 -0600

    Add Logo.jsx

---
+++ src/components/Logo.jsx
@@ -0,0 +1,15 @@
+const Logo = () => {
+    return(
+        <div className="bg-black-card p-8 text-center rounded-3xl w-8 h-8 flex justify-center items-center m-4">
+            <h1 className="text-2xl lg:hidden">
+                dW
+            </h1>
+            <h1 className="text-xl hidden lg:flex">
+                die Wortschatzkartei
+            </h1>
+        </div>
+
+    );
+}
+
+export default Logo;
--- src/components/Siderbar.jsx
+++ src/components/Siderbar.jsx
@@ -2,6 +2,12 @@
 import { Sidebar, Menu, MenuItem, SubMenu } from 'react-pro-sidebar';
 import Link from 'next/link'
 import { useState } from 'react';
+import { Menu as MenuIcon } from 'lucide-react';
+import { Library as LibraryIcon } from 'lucide-react';
+import { Mailbox as MailboxIcon } from 'lucide-react';
+import { LogIn as LogInIcon } from 'lucide-react';
+import { X as XIcon } from 'lucide-react';
+import Logo from "@/components/Logo"
 
 const Siderbar = () => {
 
@@ -10,20 +16,24 @@ const Siderbar = () => {
 
     return (
         <nav>
-            <div className="lg:hidden h-14 bg-red-card flex flex-row justify-start items-center">
-                <button className='mt-0 ml-4' onClick={ ()=>{setToggle(true)} }>T</button>
+            <div className="lg:hidden h-14 bg-blue-card flex flex-row justify-start items-center">
+                <button className='mt-0 ml-4' onClick={ ()=>{setToggle(true)} }><MenuIcon /></button>
             </div>
 
             <Sidebar rootStyles={{ height: "100vh", position: "fixed", top: 0, border: 0 }} backgroundColor={"rgb(0,120,163)"} width={"250px"} toggled={toggle} customBreakPoint={"960px"} onBackdropClick={()=>{setToggle(false)}} >
 
-                <h1>die Wortschatzkartei</h1>
+            <Logo />
+
+                <button className='lg:hidden flex'  onClick={()=>{setToggle(false)}}><XIcon size={10}/></button>
 
                 <Menu closeOnClick={true}>
-                    <MenuItem component={<Link href="/worterbuch" />}> Wörterbuch</MenuItem>
-                    <MenuItem component={<Link href="/kontakt" />}> Kontakt</MenuItem>
-                    <MenuItem component={<Link href="/login" />}> Anmelden</MenuItem>
+                    <MenuItem icon={<LibraryIcon/>} component={<Link href="/worterbuch" />}> Wörterbuch</MenuItem>
+                    <MenuItem icon={<MailboxIcon/>} component={<Link href="/kontakt" />}> Kontakt</MenuItem>
+                    <MenuItem icon={<LogInIcon/>} component={<Link href="/login" />}> Anmelden</MenuItem>
                 </Menu>
+
             </Sidebar>
+
 
         </nav>
 
