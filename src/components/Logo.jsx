b0a6dc968cae0a4128f1b6b941780d60fa96e7c1
Mario Mata <mmatas1300@gmail.com>
2024-04-26 14:10:46 -0600

    Finish sidebar

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -1,14 +1,12 @@
-const Logo = () => {
-    return(
-        <div className="bg-black-card p-8 text-center rounded-3xl w-8 h-8 flex justify-center items-center m-4">
-            <h1 className="text-2xl lg:hidden">
-                dW
-            </h1>
-            <h1 className="text-xl hidden lg:flex">
-                die Wortschatzkartei
-            </h1>
-        </div>
+import Link from "next/link";
 
+const Logo = () => {
+    return (
+        <Link className="bg-black-card text-center rounded-2xl p-2 my-4 lg:p-3 mx-auto" href="/">
+                <h1 className="text-lg lg:text-xl">
+                    die Wortschatzkartei
+                </h1>
+        </Link>
     );
 }
 
--- src/components/Siderbar.jsx
+++ src/components/Siderbar.jsx
@@ -1,42 +1,65 @@
 "use client"
-import { Sidebar, Menu, MenuItem, SubMenu } from 'react-pro-sidebar';
+import { Sidebar, Menu, MenuItem } from 'react-pro-sidebar';
 import Link from 'next/link'
 import { useState } from 'react';
-import { Menu as MenuIcon } from 'lucide-react';
-import { Library as LibraryIcon } from 'lucide-react';
-import { Mailbox as MailboxIcon } from 'lucide-react';
-import { LogIn as LogInIcon } from 'lucide-react';
-import { X as XIcon } from 'lucide-react';
+import { Menu as MenuIcon, NotebookText as LibraryIcon, Mailbox as MailboxIcon, LogIn as LogInIcon, X as XIcon, CircleUser as CircleUserIcon, SquarePlay as SquarePlayIcon, LogOut as LogOutIcon } from 'lucide-react';
 import Logo from "@/components/Logo"
+import { useSession } from 'next-auth/react'
+import { signOut } from 'next-auth/react';
+import { Spinner } from "@material-tailwind/react";
 
 const Siderbar = () => {
 
-    const [toggle,setToggle] = useState(false);
+    const [toggle, setToggle] = useState(false);
+    const { status } = useSession();
 
+    const varLinks = (status) => {
+        if (status === "authenticated") {
+            return (<>
+                <MenuItem onClick={() => { setToggle(false) }} icon={<CircleUserIcon />} component={<Link href="/konto" />}> Mein Konto</MenuItem>
+                <MenuItem onClick={() => { setToggle(false) }} icon={<SquarePlayIcon />} component={<Link href="/uben" />}> Üben</MenuItem>
+                <MenuItem onClick={() => { setToggle(false) }} icon={<LibraryIcon />} component={<Link href="/worterbuch" />}> Wörterbuch</MenuItem>
+                <MenuItem onClick={() => { setToggle(false) }} icon={<MailboxIcon />} component={<Link href="/kontakt" />}> Kontakt</MenuItem>
+                <MenuItem onClick={() => { signOut(); }} icon={<LogOutIcon />}>Abmelden</MenuItem>
+
+            </>)
+        } else if (status === "unauthenticated") {
+            return (<>
+                <MenuItem onClick={() => { setToggle(false) }} icon={<LibraryIcon />} component={<Link href="/worterbuch" />}> Wörterbuch</MenuItem>
+                <MenuItem onClick={() => { setToggle(false) }} icon={<MailboxIcon />} component={<Link href="/kontakt" />}> Kontakt</MenuItem>
+                <MenuItem onClick={() => { setToggle(false) }} icon={<LogInIcon />} component={<Link href="/login" />}> Anmelden</MenuItem>
+            </>)
+        } else {
+            return (<div className='flex justify-center items-center'><Spinner className="h-10 w-10" /></div>);
+        }
+    }
 
     return (
         <nav>
-            <div className="lg:hidden h-14 bg-blue-card flex flex-row justify-start items-center">
-                <button className='mt-0 ml-4' onClick={ ()=>{setToggle(true)} }><MenuIcon /></button>
+            <div className="lg:hidden h-14 bg-blue-card flex flex-row justify-between  items-center">
+                <button className='mt-0 ml-4' onClick={() => { setToggle(true) }}><MenuIcon /></button>
+                <div className={toggle ? "hidden" : "flex"}>
+                    <Logo />
+                </div>
+                <div className='w-[74px]' />
             </div>
 
-            <Sidebar rootStyles={{ height: "100vh", position: "fixed", top: 0, border: 0 }} backgroundColor={"rgb(0,120,163)"} width={"250px"} toggled={toggle} customBreakPoint={"960px"} onBackdropClick={()=>{setToggle(false)}} >
+            <Sidebar rootStyles={{ height: "100vh", position: "fixed", top: 0, border: 0, }} backgroundColor={"rgb(0,120,163)"} width={"250px"} toggled={toggle} customBreakPoint={"960px"} onBackdropClick={() => { setToggle(false) }} >
 
-            <Logo />
+                <div className='flex flex-row justify-between'>
+                    <Logo />
+                    <button className='lg:hidden block p-3 my-auto mx-2' onClick={() => { setToggle(false) }}><XIcon size={16} /></button>
+                </div>
 
-                <button className='lg:hidden flex'  onClick={()=>{setToggle(false)}}><XIcon size={10}/></button>
-
-                <Menu closeOnClick={true}>
-                    <MenuItem icon={<LibraryIcon/>} component={<Link href="/worterbuch" />}> Wörterbuch</MenuItem>
-                    <MenuItem icon={<MailboxIcon/>} component={<Link href="/kontakt" />}> Kontakt</MenuItem>
-                    <MenuItem icon={<LogInIcon/>} component={<Link href="/login" />}> Anmelden</MenuItem>
+                <Menu menuItemStyles={{
+                    button: {
+                        ":hover": { backgroundColor: "#4CAB2A", borderRadius: "16px", margin: "0px 20px" },
+                    }
+                }}>
+                    {varLinks(status)}
                 </Menu>
-
             </Sidebar>
-
-
         </nav>
-
     );
 };
 
