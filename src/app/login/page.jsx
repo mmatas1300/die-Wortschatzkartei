337b8dec55f01114696e0e9ebc404904a815093a
Mario Mata <mmatas1300@gmail.com>
2024-04-27 16:32:09 -0600

    Update Anmelden/registrieren

--- src/app/login/page.jsx
+++ src/app/login/page.jsx
@@ -3,7 +3,7 @@ import { Spinner } from "@material-tailwind/react";
 import style from '@/app/ui/login.module.css'
 import { useState } from "react"
 import axios from 'axios';
-import {useRouter} from "next/navigation";
+import { useRouter } from "next/navigation";
 import { signIn } from "next-auth/react";
 
 
@@ -19,7 +19,7 @@ function LoginPage() {
         if (data.get("email").length === 0) {
             setFormError("Gib deine E-Mail-Adresse ein")
             validation = false;
-        }else if (data.get("password").length<3){
+        } else if (data.get("password").length < 3) {
             setFormError("Passwörter müssen mindestens 3 Zeichen lang sein")
             validation = false;
         } else if (data.get("password") !== data.get("confirmPassword")) {
@@ -43,15 +43,15 @@ function LoginPage() {
                 console.log(res)
                 setFormError()
                 setButtonState(<div className={style['success']}>Erfolgreiche Registrierung!</div>);
-                setTimeout(function() {
+                setTimeout(function () {
                     setFormState(style["container"])
                     setButtonState(<button>Weiter</button>)
-                  }, 1500);
-                
+                }, 1500);
+
             } catch (error) {
                 setFormError(error.response.data.message)
             }
-        } else{
+        } else {
             setButtonState(<button>Weiter</button>);
         }
 
@@ -67,9 +67,9 @@ function LoginPage() {
                 password: formData.get("password"),
                 redirect: false
             });
-            if(res.ok) return router.push("/konto")
-            setFormError(res.error) 
-            
+            if (res.ok) return router.push("/konto")
+            setFormError(res.error)
+
         } catch (error) {
             console.log(error)
         }
@@ -77,54 +77,58 @@ function LoginPage() {
     };
 
     return (
-        <div className={style["section"]}>
-            <div className={formState}>
-                <div className={`${style["form-container"]} ${style["sign-up"]}`}>
-                    <form onSubmit={handleRegistrierenSubmit}>
-                        <h1 className="mb-1">Registrieren</h1>
-                        <p className="mb-1">Es geht ganz schnell und einfach.</p>
-                        <label htmlFor="email">Deine E-Mail-Adresse:</label>
-                        <input type="email" placeholder="E-Mail-Adresse" name="email" />
-                        <label htmlFor="password">Neues Passwort:</label>
-                        <input type="password" placeholder="Neues Passwort" name="password" />
-                        <label htmlFor="confirmPassword">Passwort nochmals eingeben:</label>
-                        <input type="password" placeholder="Passwort nochmals eingeben" name="confirmPassword" />
-                        {formError && <div className={style['error']}>{formError}</div>}
-                        {buttonState}
-                    </form>
-                </div>
-                <div className={`${style["form-container"]} ${style["sign-in"]}`}>
-                    <form onSubmit={handleAnmeldenSubmit}>
-                        <h1 className="mb-2">Anmelden</h1>
-                        <p className="mb-2">Willkommen zurück!</p>
-                        <label htmlFor="email">Deine E-Mail-Adresse:</label>
-                        <input type="email" placeholder="E-Mail-Adresse" name="email" />
-                        <label htmlFor="password">Dein Passwort:</label>
-                        <input type="password" placeholder="Passwort" name="password" />
-                        {formError && <div className={style['error']}>{formError}</div>}
-                        {buttonState}
-                    </form>
-                </div>
-                <div className={style["toggle-container"]}>
-                    <div className={style["toggle"]}>
-                        <div className={`${style["toggle-panel"]} ${style["toggle-left"]}`}>
-                            <h1 className="mb-2">Willkommen zurück!</h1>
-                            <p className="mb-2">Du hast bereits ein Konto?</p>
-                            <button onClick={() => { setFormError(); setFormState(style["container"]) }}>
-                                Anmelden
-                            </button>
-                        </div>
-                        <div className={`${style["toggle-panel"]} ${style["toggle-right"]}`}>
-                            <h1 className="mb-2">Willkommen!</h1>
-                            <p className="mb-2">Du hast kein Konto?</p>
-                            <button onClick={() => { setFormError(); setFormState(`${style["container"]} ${style["active"]}`) }}>
-                                Registrieren
-                            </button>
+        <section className="my-12">
+            <h1 className="text-center">Anmelden/Registrieren</h1>
+            <div className={style["section"]}>
+                <div className={formState}>
+                    <div className={`${style["form-container"]} ${style["sign-up"]}`}>
+                        <form onSubmit={handleRegistrierenSubmit}>
+                            <h1 className="mb-1">Registrieren</h1>
+                            <p className="mb-1">Es geht ganz schnell und einfach.</p>
+                            <label htmlFor="email">Deine E-Mail-Adresse:</label>
+                            <input type="email" placeholder="E-Mail-Adresse" name="email" />
+                            <label htmlFor="password">Neues Passwort:</label>
+                            <input type="password" placeholder="Neues Passwort" name="password" />
+                            <label htmlFor="confirmPassword">Passwort nochmals eingeben:</label>
+                            <input type="password" placeholder="Passwort nochmals eingeben" name="confirmPassword" />
+                            {formError && <div className={style['error']}>{formError}</div>}
+                            {buttonState}
+                        </form>
+                    </div>
+                    <div className={`${style["form-container"]} ${style["sign-in"]}`}>
+                        <form onSubmit={handleAnmeldenSubmit}>
+                            <h1 className="mb-2">Anmelden</h1>
+                            <p className="mb-2">Willkommen zurück!</p>
+                            <label htmlFor="email">Deine E-Mail-Adresse:</label>
+                            <input type="email" placeholder="E-Mail-Adresse" name="email" required/>
+                            <label htmlFor="password">Dein Passwort:</label>
+                            <input type="password" placeholder="Passwort" name="password" required/>
+                            {formError && <div className={style['error']}>{formError}</div>}
+                            {buttonState}
+                        </form>
+                    </div>
+                    <div className={style["toggle-container"]}>
+                        <div className={style["toggle"]}>
+                            <div className={`${style["toggle-panel"]} ${style["toggle-left"]}`}>
+                                <h1 className="mb-2">Willkommen zurück!</h1>
+                                <p className="mb-2">Du hast bereits ein Konto?</p>
+                                <button onClick={() => { setFormError(); setFormState(style["container"]) }}>
+                                    Anmelden
+                                </button>
+                            </div>
+                            <div className={`${style["toggle-panel"]} ${style["toggle-right"]}`}>
+                                <h1 className="mb-2">Willkommen!</h1>
+                                <p className="mb-2">Du hast kein Konto?</p>
+                                <button onClick={() => { setFormError(); setFormState(`${style["container"]} ${style["active"]}`) }}>
+                                    Registrieren
+                                </button>
+                            </div>
                         </div>
                     </div>
                 </div>
             </div>
-        </div>
+        </section>
+
 
     )
 }
