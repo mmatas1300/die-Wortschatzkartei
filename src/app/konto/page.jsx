68d9120a7d30c9b75a6cf7f38a979a2fe07dc318
Mario Mata <mmatas1300@gmail.com>
2024-04-27 19:15:16 -0600

    Update Konto

--- src/app/konto/page.jsx
+++ src/app/konto/page.jsx
@@ -2,21 +2,24 @@
 import { useSession } from 'next-auth/react'
 import { Spinner } from "@material-tailwind/react";
 import axios from 'axios';
+import { useState } from 'react';
 
 function KontoPage() {
 
+    const fertigButton = (<button className='mb-3'>Fertig</button>)
     const { data: session, status, update } = useSession();
+    const [stateButton, setStateButton] = useState(fertigButton);
 
     const handleKontoeinstellungen = async (e) => {
         e.preventDefault()
+        setStateButton(<Spinner className="mt-2.5 mb-4 h-8 w-8" />)
         const formData = new FormData(e.currentTarget)
-
         const config = {
             nick: formData.get('nick') ? formData.get('nick') : session.user.config.nick,
             cardsSet: formData.get('cardsSet')
         }
         try {
-            const userData = await axios({
+            await axios({
                 method: 'put',
                 url: '/api/user/config',
                 data: {
@@ -25,35 +28,32 @@ function KontoPage() {
                 }
             });
             await update({ user: { ...session.user, config: config } })//Actualizar la session
-            console.log(userData)
+            setStateButton(fertigButton);
         } catch (error) {
             console.log(error)
         }
-
     }
 
-    console.log(session, status);
-
     return (
-
         <section className='flex flex-col justify-center items-center mx-auto my-12'>
             {status === "loading" ? (<Spinner className="mt-2.5 h-10 w-10" />) :
                 (<>
                     <h1 className='text-2xl mb-4'>Mein Konto</h1>
                     <h1 className='text-xl mb-4'>{session.user.config.nick ? "Willkommen " + session.user.config.nick + "!" : "Willkommen, richten Sie bitte Ihr Konto ein!"}</h1>
-                    <div className='bg-green-card rounded-3xl lg:-rotate-12'>
+                    <div className='bg-green-card rounded-3xl lg:-rotate-12 lg:mt-8'>
                         <div className='bg-blue-card rounded-3xl lg:rotate-6'>
                             <div className={`bg-red-card w-96 rounded-3xl p-8 flex flex-col justify-center items-center lg:rotate-6`}>
+                                <h1>Kontoeinstellungen</h1>
                                 <form className='mt-4 flex flex-col justify-center items-center' onSubmit={handleKontoeinstellungen}>
                                     <label htmlFor="nick">Spitzname:</label>
                                     <input type="text" placeholder={session.user.config.nick} name='nick' />
-                                    <label htmlFor="cardsSet">Möchtest du lieber deine eigenen Karten verwenden oder die der App?</label>
+                                    <label className='mt-2' htmlFor="cardsSet">Möchtest du lieber deine eigenen Karten verwenden oder die der App?</label>
                                     <select name="cardsSet">
                                         {session.user.config.cardsSet === "app" ? (<><option value="app">App-Karten</option>
                                             <option value="meine">Meine Karten</option></>) : (<><option value="meine">Meine Karten</option><option value="app">App-Karten</option>
                                             </>)}
                                     </select>
-                                    <button className='mb-3'>Fertig</button>
+                                    {stateButton}
                                 </form>
                             </div>
                         </div>
@@ -62,7 +62,6 @@ function KontoPage() {
 
                 </>)}
         </section>
-
     )
 }
 
