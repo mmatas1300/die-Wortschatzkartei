cbb469fcd402aeab5609a0aff35fe629588c1c99
Mario Mata <mmatas1300@gmail.com>
2024-05-02 22:44:36 -0600

    Add PlayScreen

--- src/components/uben/PlayScreen.jsx
+++ src/components/uben/PlayScreen.jsx
@@ -1,22 +1,23 @@
 import { useSession } from "next-auth/react";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
+import { Spinner } from "@material-tailwind/react";
+import Karte from "../Karte";
 
-const PlayScreen = ({ cards, progress, set }) => {
+const PlayScreen = ({ cards, progress }) => {
 
     const { data: session } = useSession();
+    const [selectedCards, setSelectedCards] = useState([]);
+    const [startGame, setStartGame] = useState();
 
     useEffect(() => {
 
-
-        const shuffleArray = (array)=> {
+        const shuffleArray = (array) => {
             for (let i = array.length - 1; i > 0; i--) {
                 const j = Math.floor(Math.random() * (i + 1));
                 [array[i], array[j]] = [array[j], array[i]];
             }
             return array;
         };
-
-
 
         if (session.user.config.cardsSet === "app") {
             //Obtener las tarjetas a estudiar
@@ -28,29 +29,30 @@ const PlayScreen = ({ cards, progress, set }) => {
                 const newCardsDate = new Date("2000");
                 return cardPracticeDate.getTime() === newCardsDate.getTime();
             });
-            const fiveNewCards = shuffleArray(newCards).slice(0,5);
+            const fiveNewCards = shuffleArray(newCards).slice(0, 5);
 
-            const toStudyCards = cards.filter((card)=>{
+            const toStudyCards = cards.filter((card) => {
                 const cardPracticeDate = new Date(card.practiceDate);
                 cardPracticeDate.setDate(cardPracticeDate.getDate() + 1);
                 const today = new Date();
                 const newCardsDate = new Date("2000-01-02");
-                return (cardPracticeDate.getTime() < today.getTime()) && (cardPracticeDate.getTime() != newCardsDate.getTime()) ;
+                return (cardPracticeDate.getTime() < today.getTime()) && (cardPracticeDate.getTime() != newCardsDate.getTime());
             })
 
             const todayCards = shuffleArray(toStudyCards.concat(fiveNewCards));
-            console.log(todayCards)
+            setSelectedCards(todayCards);
         }
-
-
-
-
     }, [])
 
+
     return (
-        <div>
-            {JSON.stringify(cards)}
-        </div>
+        selectedCards.length===0 ? (<Spinner className="mt-2.5 h-10 w-10" />) : 
+        (
+            <div className="flex flex-col justify-center items-center">
+                <h2>Es gibt heute {selectedCards.length} drei Karten zu studieren</h2>
+                <button onClick={()=>setStartGame()}>Los geht's!</button>
+            </div>
+        )
     );
 }
 
--- src/components/uben/UbenMessages.jsx
+++ src/components/uben/UbenMessages.jsx
@@ -6,7 +6,7 @@ const UbenMessages = ({message}) => {
                 <div className="bg-green-card -rotate-10 rounded-2xl">
                     <div className="bg-red-card -rotate-10 rounded-2xl">
                         <div className="bg-orange-card px-10 py-16 rounded-2xl -rotate-10">
-                            <h2>{message}</h2>
+                            <div className="text-xl">{message}</div>
                         </div>
                     </div>
                 </div>
