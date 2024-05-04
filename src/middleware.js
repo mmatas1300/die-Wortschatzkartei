81368469d2cf84af1d7156d5cfc14e13395a3bcf
Mario Mata <mmatas1300@gmail.com>
2024-05-04 11:36:16 -0600

    Add user route

--- src/middleware.js
+++ src/middleware.js
@@ -1,5 +1,5 @@
 export {default} from 'next-auth/middleware';
 
 export const config ={
-    matcher: ['/konto','/karteneditor','/uben']
+    matcher: ['/konto','/karteneditor','/uben','/api/user/:path']
 };
