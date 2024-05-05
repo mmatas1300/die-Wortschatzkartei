ed455bc1dd791b0e482412a3fdd6ce462cd69172
Mario Mata <mmatas1300@gmail.com>
2024-05-05 13:31:21 -0600

    Fix style in karteneditor

--- src/components/kartenenditor/EditCardForm.jsx
+++ src/components/kartenenditor/EditCardForm.jsx
@@ -90,54 +90,54 @@ const EditCardForm = () => {
         <div className='grid grid-cols-2 lg:grid-cols-3 w-full bg-black-card/[0.1] rounded-2xl lg:rounded-3xl ps-2 lg:ps-4 py-2'>
             <div className='flex flex-col justify-center items-center lg:order-1'>
                 <label htmlFor="ps1">Ich:</label>
-                <input className='max-w-32' type="text" placeholder='Bin' name='ps1' defaultValue={card.type === "Verb" ? card.prasens[0] : null} />
+                <input className='max-w-20 lg:max-w-32' type="text" placeholder='Bin' name='ps1' defaultValue={card.type === "Verb" ? card.prasens[0] : null} />
             </div>
             <div className='flex flex-col justify-center items-center  lg:order-4'>
                 <label htmlFor="ps4">Wir:</label>
-                <input className='max-w-32' type="text" placeholder='Sind' name='ps4' defaultValue={card.type === "Verb" ? card.prasens[3] : null} />
+                <input className='max-w-20 lg:max-w-32' type="text" placeholder='Sind' name='ps4' defaultValue={card.type === "Verb" ? card.prasens[3] : null} />
             </div>
             <div className='flex flex-col justify-center items-center lg:order-2'>
                 <label htmlFor="ps2">Du:</label>
-                <input className='max-w-32' type="text" placeholder='Bist' name='ps2' defaultValue={card.type === "Verb" ? card.prasens[1] : null} />
+                <input className='max-w-20 lg:max-w-32' type="text" placeholder='Bist' name='ps2' defaultValue={card.type === "Verb" ? card.prasens[1] : null} />
             </div>
             <div className='flex flex-col justify-center items-center lg:order-5'>
                 <label htmlFor="ps5">Ihr:</label>
-                <input className='max-w-32' type="text" placeholder='Seid' name='ps5' defaultValue={card.type === "Verb" ? card.prasens[4] : null} />
+                <input className='max-w-20 lg:max-w-32' type="text" placeholder='Seid' name='ps5' defaultValue={card.type === "Verb" ? card.prasens[4] : null} />
             </div>
             <div className='flex flex-col justify-center items-center lg:order-3'>
                 <label htmlFor="ps3">Er/Sie/Es:</label>
-                <input className='max-w-32' type="text" placeholder='Ist' name='ps3' defaultValue={card.type === "Verb" ? card.prasens[2] : null} />
+                <input className='max-w-20 lg:max-w-32' type="text" placeholder='Ist' name='ps3' defaultValue={card.type === "Verb" ? card.prasens[2] : null} />
             </div>
             <div className='flex flex-col justify-center items-center lg:order-6'>
                 <label htmlFor="ps6">Sie:</label>
-                <input className='max-w-32' type="text" placeholder='Sind' name='ps6' defaultValue={card.type === "Verb" ? card.prasens[5] : null} />
+                <input className='max-w-20 lg:max-w-32' type="text" placeholder='Sind' name='ps6' defaultValue={card.type === "Verb" ? card.prasens[5] : null} />
             </div>
         </div>
         <span className='text-sm underline mt-1'>Präteritum</span>
         <div className='grid grid-cols-2 lg:grid-cols-3 w-full bg-black-card/[0.1] rounded-2xl lg:rounded-3xl ps-2 lg:ps-4 py-2'>
             <div className='flex flex-col justify-center items-center lg:order-1'>
                 <label htmlFor="pm1">Ich:</label>
-                <input className='max-w-32' type="text" placeholder='War' name='pm1' defaultValue={card.type === "Verb" ? card.prateritum[0] : null} />
+                <input className='max-w-20 lg:max-w-32' type="text" placeholder='War' name='pm1' defaultValue={card.type === "Verb" ? card.prateritum[0] : null} />
             </div>
             <div className='flex flex-col justify-center items-center lg:order-4'>
                 <label htmlFor="pm4">Wir:</label>
-                <input className='max-w-32' type="text" placeholder='Waren' name='pm4' defaultValue={card.type === "Verb" ? card.prateritum[3] : null} />
+                <input className='max-w-20 lg:max-w-32' type="text" placeholder='Waren' name='pm4' defaultValue={card.type === "Verb" ? card.prateritum[3] : null} />
             </div>
             <div className='flex flex-col justify-center items-center lg:order-2'>
                 <label htmlFor="pm2">Du:</label>
-                <input className='max-w-32' type="text" placeholder='Warst' name='pm2' defaultValue={card.type === "Verb" ? card.prateritum[1] : null} />
+                <input className='max-w-20 lg:max-w-32' type="text" placeholder='Warst' name='pm2' defaultValue={card.type === "Verb" ? card.prateritum[1] : null} />
             </div>
             <div className='flex flex-col justify-center items-center lg:order-5'>
                 <label htmlFor="pm5">Ihr:</label>
-                <input className='max-w-32' type="text" placeholder='Wart' name='pm5' defaultValue={card.type === "Verb" ? card.prateritum[4] : null} />
+                <input className='max-w-20 lg:max-w-32' type="text" placeholder='Wart' name='pm5' defaultValue={card.type === "Verb" ? card.prateritum[4] : null} />
             </div>
             <div className='flex flex-col justify-center items-center lg:order-3'>
                 <label htmlFor="pm3">Er/Sie/Es:</label>
-                <input className='max-w-32' type="text" placeholder='War' name='pm3' defaultValue={card.type === "Verb" ? card.prateritum[2] : null} />
+                <input className='max-w-20 lg:max-w-32' type="text" placeholder='War' name='pm3' defaultValue={card.type === "Verb" ? card.prateritum[2] : null} />
             </div>
             <div className='flex flex-col justify-center items-center lg:order-6'>
                 <label htmlFor="pm6">Sie:</label>
-                <input className='max-w-32' type="text" placeholder='Waren' name='pm6' defaultValue={card.type === "Verb" ? card.prateritum[5] : null} />
+                <input className='max-w-20 lg:max-w-32' type="text" placeholder='Waren' name='pm6' defaultValue={card.type === "Verb" ? card.prateritum[5] : null} />
             </div>
         </div>
         <div className='lg:grid lg:grid-cols-2 lg:w-full mt-2'>
@@ -168,19 +168,19 @@ const EditCardForm = () => {
         <div className='grid grid-cols-2 w-full'>
             <div className='flex flex-col justify-center items-center'>
                 <label htmlFor="wortMUF">Maskulim:</label>
-                <input className='max-w-32 lg:max-w-56' type="text" placeholder='Koch' name='wortMUF' required defaultValue={card.wort} />
+                <input className='max-w-20 lg:max-w-56' type="text" placeholder='Koch' name='wortMUF' required defaultValue={card.wort} />
             </div>
             <div className='flex flex-col justify-center items-center'>
                 <label htmlFor="frau">Feminin:</label>
-                <input className='max-w-32 lg:max-w-56' type="text" placeholder='Köchin' name="frau" defaultValue={card.frau} />
+                <input className='max-w-20 lg:max-w-56' type="text" placeholder='Köchin' name="frau" defaultValue={card.frau} />
             </div>
             <div className='flex flex-col justify-center items-center'>
                 <label htmlFor="manner">Plural (M):</label>
-                <input className='max-w-32 lg:max-w-56' type="text" placeholder='Köche' name="manner" defaultValue={card.manner} />
+                <input className='max-w-20 lg:max-w-56' type="text" placeholder='Köche' name="manner" defaultValue={card.manner} />
             </div>
             <div className='flex flex-col justify-center items-center'>
                 <label htmlFor="frauen">Plural (F):</label>
-                <input className='max-w-32 lg:max-w-56' type="text" placeholder='Köchinnen' name='frauen' defaultValue={card.frauen} />
+                <input className='max-w-20 lg:max-w-56' type="text" placeholder='Köchinnen' name='frauen' defaultValue={card.frauen} />
             </div>
         </div>
         <div className='lg:grid lg:grid-cols-2 lg:w-full'>
