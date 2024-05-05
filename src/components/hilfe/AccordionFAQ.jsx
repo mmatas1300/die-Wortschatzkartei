8d53b236238f58ea3ea208d6754f7e555a47a5bb
Mario Mata <mmatas1300@gmail.com>
2024-05-04 19:39:31 -0600

    Fix format in FAQ

--- src/components/hilfe/AccordionFAQ.jsx
+++ src/components/hilfe/AccordionFAQ.jsx
@@ -12,49 +12,49 @@ const AccordionFAQ = () => (
     >
         <div className='ms-6 text-lg'>FAQ</div>
         <AccordionItem value="item-1">
-            <AccordionTrigger>What is &aposdie Wortschatzkartei&apos ?</AccordionTrigger>
-            <AccordionContent>&aposdie Wortschatzkartei&apos is a web application designed to create your own vocabulary flashcards and learn German vocabulary with spaced repetition.</AccordionContent>
+            <AccordionTrigger>What is &apos;die Wortschatzkartei&apos; ?</AccordionTrigger>
+            <AccordionContent>&apos;die Wortschatzkartei&apos; is a web application designed to create your own vocabulary flashcards and learn German vocabulary with spaced repetition.</AccordionContent>
         </AccordionItem>
 
         <AccordionItem value="item-2">
             <AccordionTrigger>How can I get started?</AccordionTrigger>
             <AccordionContent>
-            Create an account by clicking &aposAnmelden&apos, select the &aposRegistrieren&apos button and enter your email and password. Once you are registered, you can log in using the &aposAnmelden&apos section of the form.
+            Create an account by clicking &apos;Anmelden&apos;, select the &apos;Registrieren&apos; button and enter your email and password. Once you are registered, you can log in using the &apos;Anmelden&apos; section of the form.
             </AccordionContent>
         </AccordionItem>
 
         <AccordionItem value="item-3">
-            <AccordionTrigger>What do I do once I&aposm logged in?</AccordionTrigger>
+            <AccordionTrigger>What do I do once I&apos;m logged in?</AccordionTrigger>
             <AccordionContent>
-            Once you&aposve logged in, set up your account with your nickname (Spitzname) and choose whether you want to use your own cards or the ones loaded by default. You can change these settings whenever you want in the &aposMein Konto&apos section.
+            Once you&apos;ve logged in, set up your account with your nickname (Spitzname) and choose whether you want to use your own cards or the ones loaded by default. You can change these settings whenever you want in the &apos;Mein Konto&apos; section.
             </AccordionContent>
         </AccordionItem>
 
         <AccordionItem value="item-4">
             <AccordionTrigger>How can I create my own cards?</AccordionTrigger>
             <AccordionContent>
-            You can find the &aposNeue Karte erstellen&apos form in the &aposKarteneditor&apos section. In this form, you can choose the type of word you want to add. You can add nouns (Nomen), verbs (Verb), words that have variations for men and women (Gleichstellungsnomen), and any other category (Andere Wort).
+            You can find the &apos;Neue Karte erstellen&apos; form in the &apos;Karteneditor&apos; section. In this form, you can choose the type of word you want to add. You can add nouns (Nomen), verbs (Verb), words that have variations for men and women (Gleichstellungsnomen), and any other category (Andere Wort).
             </AccordionContent>
         </AccordionItem>
 
         <AccordionItem value="item-5">
             <AccordionTrigger>Where can I see my cards?</AccordionTrigger>
             <AccordionContent>
-            You can find your cards sorted in the &aposWörterbuch&apos section. In order to view your cards, it&aposs necessary that you have previously configured your account to use your own cards. Otherwise, you will see the default cards.
+            You can find your cards sorted in the &apos;Wörterbuch&apos; section. In order to view your cards, it&apos;s necessary that you have previously configured your account to use your own cards. Otherwise, you will see the default cards.
             </AccordionContent>
         </AccordionItem>
 
         <AccordionItem value="item-6">
             <AccordionTrigger>How can I edit my cards?</AccordionTrigger>
             <AccordionContent>
-            In the &aposKarteneditor&apos section, you will find the &aposKarten verwalten&apos form, where you can manage your cards. You can edit them, reset their progress, or delete them.
+            In the &apos;Karteneditor&apos; section, you will find the &apos;Karten verwalten&apos; form, where you can manage your cards. You can edit them, reset their progress, or delete them.
             </AccordionContent>
         </AccordionItem>
 
         <AccordionItem value="item-7">
             <AccordionTrigger>How can I study the cards?</AccordionTrigger>
             <AccordionContent>
-            In the &aposÜben&apos section, you can start your daily practice. When a card is presented to you, you should remember its meaning and additional information. If you were able to recall it, click the &aposRichtig&apos button; otherwise, click &aposFalsch&apos. Remember that the cards you practice with depend on the settings established in the &aposMein Konto&apos section.
+            In the &apos;Üben&apos; section, you can start your daily practice. When a card is presented to you, you should remember its meaning and additional information. If you were able to recall it, click the &apos;Richtig&apos; button; otherwise, click &apos;Falsch&apos;. Remember that the cards you practice with depend on the settings established in the &apos;Mein Konto&apos; section.
             </AccordionContent>
         </AccordionItem>
     </Accordion.Root>
