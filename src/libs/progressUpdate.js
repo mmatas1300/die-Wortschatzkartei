2a8ad54bad24aabdf9ff15f10b758bc644608339
Mario Mata <mmatas1300@gmail.com>
2024-05-05 18:46:43 -0600

    Remove console.log

--- src/libs/progressUpdate.js
+++ src/libs/progressUpdate.js
@@ -4,7 +4,6 @@ const progressUpdate = (cards,userProgress)=>{
         const newCard = cards.filter((card)=>{
             return card._id != element.cardId;
         });
-        console.log(newCard[0])
         if(newCard[0]){
             const newProgress = {cardId: newCard[0]._id, level:0, practiceDate: new Date("2000")};
             newUserProgress.push(newProgress);
