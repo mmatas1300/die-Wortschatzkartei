7e52ab257753b20a681c6bbdb4e5109e6d97b9f4
Mario Mata <mmatas1300@gmail.com>
2024-05-16 16:18:11 -0600

    Add Image in Karte

--- src/components/Karte.jsx
+++ src/components/Karte.jsx
@@ -1,4 +1,5 @@
 'use client'
+import Image from 'next/image';
 import { useEffect, useState } from 'react';
 
 function Karte(karte) {
@@ -102,8 +103,9 @@ function Karte(karte) {
                 <p className='underline mt-2'>{karte.type === "Verb" ? "Partizip II" : ""}</p>
                 <p className='text-sm'>{karte.partizip2}</p></div>) : ""}
 
-
-            <img src={karte.bild} alt={wortKarte()} className='w-60 rounded-2xl my-4' />
+            <div className='w-60 h-auto my-4'>
+                <Image src={karte.bild} alt={wortKarte()} width={240} height={240} className='rounded-2xl' />
+            </div>
             <p className='mb-4'>{karte.verwandte}</p>
             <p className='mx-4 mb-4 text-center'>{karte.beispiel}</p>
             <p onClick={toggleUbersetzung} className='mb-4 bg-black-card p-1 rounded-md cursor-pointer text-sm'>{showUbersetzung ? karte.ubersetzung : "Übersetzung"}</p>
