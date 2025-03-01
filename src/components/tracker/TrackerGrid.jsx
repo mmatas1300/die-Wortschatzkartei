519f9a910077921230a4c56749228b66b8f281e4
Mario Mata <mmatas1300@gmail.com>
2025-03-01 11:16:56 -0600

    Add tracker info

--- src/components/tracker/TrackerGrid.jsx
+++ src/components/tracker/TrackerGrid.jsx
@@ -1,12 +1,38 @@
-const TrackerGrid = ({streak}) => {
-    return(
-        
+const TrackerGrid = ({ streak }) => {
+
+    const gridDays = new Array(120);
+    const streakReverse = streak.toReversed();
+
+    for (let i = 0; i < gridDays.length; i++) {
+        gridDays[i] = { date: new Date(), cardsPlayed: 0 };
+        gridDays[i].date = new Date(gridDays[i].date.setDate(gridDays[i].date.getDate() - i));
+    }
+
+    gridDays.forEach((gridDay) => {
+        for (let i = 0; i < streakReverse.length; i++) {
+
+            const streakDayDate = new Date(streakReverse[i].dayPlayed);
+            const gridDayDate = new Date(gridDay.date);
+
+            if(streakDayDate<gridDays[gridDays.length-1].date){
+                break;
+            }
+
+            if( gridDayDate.getFullYear() == streakDayDate.getFullYear() & gridDayDate.getDate() == streakDayDate.getDate() & gridDayDate.getMonth() == streakDayDate.getMonth() ){
+                gridDay.cardsPlayed = streakReverse[i].cardsPlayed;
+                break;
+            }
+        }
+    })
+
+    return (
+
         <div className="bg-light-green-800 flex">
             {
-                streak.map((day)=>{
-                    return(
+                gridDays.map((day) => {
+                    return (
                         <div className="m-1 bg-cyan-300">
-                            {day.dayPlayed}
+                            {day.cardsPlayed}
                         </div>
                     )
                 })
